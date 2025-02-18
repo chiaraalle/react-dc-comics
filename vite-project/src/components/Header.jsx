0b37a8d93aac90f4004065a9b974613db40b795e
chiaralle <chiaraallegrino@gmail.com>
2025-02-18 20:06:09 +0100

    add: Main.jsx

---
+++ vite-project/src/components/Header.jsx
@@ -0,0 +1,8 @@
+//i componenti sono delle funzioni che possiamo scrivere nel metodo classico o in arrow function
+//ci deve essere sempre il return e dentro le parentesi andremo a scrivere la logica
+function Header() {
+
+    return (
+      <h1>HELLO WORLD</h1>
+    )
+  }
