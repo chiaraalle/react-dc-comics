3c5c407cf2113b0cbf5b9f4227cdc8f9e041be91
chiaralle <chiaraallegrino@gmail.com>
2025-02-19 15:34:24 +0100

    add: Header with css

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,7 +1,19 @@
+{/*Descrizione:
+Create un nuovo progetto React e definite i componenti necessari per strutturare il layout come da screenshot allegato.
+Per oggi diamo priorità alla struttura: quando è tutto bello solido, passiamo al CSS!
+Note:
+- Il font utilizzato è Open Sans
+Bonus:
+- Creare un componente aggiuntivo per gestire la fascia azzurra con le icone. */}
+
+import Header from "./components/Header";
+
 function App() {
 
   return (
-    <h1>HELLO WORLD</h1>
+    <>
+    <Header />
+    </>
   )
 }
 
--- vite-project/src/components/Header.jsx
+++ vite-project/src/components/Header.jsx
@@ -3,6 +3,27 @@
 function Header() {
 
     return (
-      <h1>HELLO WORLD</h1>
+      <header>
+        <div className="container">
+          <figure>
+            <img src="./src/assets/img/dc-logo.png" alt="" />
+          </figure>
+
+          <ul>
+            <li><a href="">CHARACTERS</a></li>
+            <li><a href="">COMICS</a></li>
+            <li><a href="">MOVIES</a></li>
+            <li><a href="">TV</a></li>
+            <li><a href="">GAMES</a></li>
+            <li><a href="">COLLECTIBLES</a></li>
+            <li><a href="">VIDEOS</a></li>
+            <li><a href="">FANS</a></li>
+            <li><a href="">NEWS</a></li>
+            <li><a href="">SHOP</a></li>
+          </ul>
+        </div>
+      </header>  
     )
   }
+
+  export default Header
